44f0ecd10593a218547e8382e1f3b62dd1343639
Dominik Haeusser <haeusser-dominik@web.de>
2021-07-09 21:37:25 +0200

    flash form content at the top

--- flask_website/singleapp.py
+++ flask_website/singleapp.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flask import request, flash
 from flask.templating import render_template
 app = Flask(__name__)
-
+app.config.from_mapping(
+        SECRET_KEY='dev')
 
 buttons = [
     {
@@ -23,24 +25,36 @@ buttons = [
 ]
 
 
-@app.route("/")
-@app.route("/home")
+def post_request():
+    if request.method == 'POST':
+        email = request.form['email']
+        question = request.form['question']
+        flash(email)
+        flash(question)
+
+
+@app.route("/", methods=('GET', 'POST'))
+@app.route("/home", methods=('GET', 'POST'))
 def home():
+    post_request()
     return render_template("home.html", buttons=buttons)
 
 
-@app.route("/piano_lesson")
+@app.route("/piano_lesson", methods=('GET', 'POST'))
 def piano_lesson():
+    post_request()
     return render_template("piano_lesson.html", title="Klavierunterricht", buttons=buttons)
 
 
-@app.route("/drums_lesson")
+@app.route("/drums_lesson", methods=('GET', 'POST'))
 def drums_lesson():
+    post_request()
     return render_template("drums_lesson.html", title="Schlagzeugunterricht", buttons=buttons)
 
 
-@app.route("/contact")
+@app.route("/contact", methods=('GET', 'POST'))
 def contact():
+    post_request()
     return render_template("contact.html", title="Kontakt", buttons=buttons)
 
 
